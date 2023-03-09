9e627d323dc265e2acbbe4022192e0225287ccee
Ivan Dujaut <dujautivan@gmail.com>
2023-03-09 11:42:30 -0300

    [NEW] - String interpolation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'my-store2';
+  name = 'Ivan'; // 👈 public
+  age = 28;
+  img = 'https://www.w3schools.com/howto/img_avatar.png';
 }
