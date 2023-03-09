499ed1d059e47dd87a3072c6a4f751def93109b0
Ivan Dujaut <dujautivan@gmail.com>
2023-03-09 12:29:15 -0300

    [NEW] - Property Binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,4 +9,10 @@ export class AppComponent {
   name = 'Ivan'; // 👈 public
   age = 28;
   img = 'https://www.w3schools.com/howto/img_avatar.png';
+  btnDisabled = true;
+  person = {
+    name: 'Ivan',
+    age: 28,
+    avatar: 'https://www.w3schools.com/howto/img_avatar.png',
+  }
 }
