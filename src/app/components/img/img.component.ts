c7cb209bf0caa65d8f48fcb72b14576e9ffb7e24
Ivan Dujaut <dujautivan@gmail.com>
2023-03-12 22:18:05 -0300

    [NEW] - Ciclo de vida de componentes

--- src/app/components/img/img.component.ts
+++ src/app/components/img/img.component.ts
@@ -1,19 +1,46 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, OnChanges, SimpleChanges, AfterViewInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-img',
   templateUrl: './img.component.html',
   styleUrls: ['./img.component.scss']
 })
-export class ImgComponent implements OnInit {
+export class ImgComponent implements OnInit, OnChanges, AfterViewInit, OnDestroy {
 
   @Input() img: string = '';
   @Output() loaded = new EventEmitter<string>();
   imgDefault = '../../../assets/images/default.png';
 
-  constructor() { }
+  constructor() {
+    //before render
+    //No async -- once time
+    console.log('constructor', 'imgValue => ', this.img);
+  }
+
+  ngOnChanges(): void {
+    // before - during render
+    // changes input -- time
+    console.log('ngOnChanges', 'imgValue => ', this.img);
+  }
 
   ngOnInit(): void {
+    // before render
+    // async - fetch, promisses -- once time. Cuando hacemos una
+    // llamada a la API, el componente alista los datos para usarlos
+    console.log('ngOnInit', 'imgValue => ', this.img);
+  }
+
+  ngAfterViewInit(): void {
+    // after render
+    // handler children. Se ven en directivas. Corre despues del
+    // render, normalmente manejamos los hijos de forma programatica
+    // y no del template, este deberia ser donde hacer eso
+    console.log('ngAfterViewInit');
+  }
+
+  ngOnDestroy(): void {
+    // delete
+    console.log('ngOnDestroy');
   }
 
   public imgError(): void {
